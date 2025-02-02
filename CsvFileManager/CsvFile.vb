69846d3bfcd30cf7ef51fe6b033be701196a8d1f
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-02-02 17:13:36 +0100

    Aktualisierung und Erweiterung der CSV-Verwaltung
    Die Datei `CsvFile.vb` wurde aktualisiert, um die Klasse `CsvFile` zu definieren, die Methoden zum Verwalten von CSV-Dateien bereitstellt. Die Klasse implementiert das `IDisposable`-Interface zur ordnungsgemäßen Freigabe von Ressourcen. Die Methoden und Eigenschaften der Klasse wurden dokumentiert und einige Methoden wurden von Instanzmethoden zu statischen Methoden geändert.
    
    Die Datei `CsvFileManager.vbproj` wurde aktualisiert, um die Assembly- und Dateiversionen auf `1.2025.0202.1656` zu ändern.
    
    Eine neue Datei `HelperFuctions.vb` wurde hinzugefügt, die Hilfsfunktionen für das Verwalten von CSV-Dateien bereitstellt. Diese Funktionen umfassen das Erstellen, Lesen und Schreiben von CSV-Dateien.
    
    Eine neue Datei `VariableDefinitions.vb` wurde hinzugefügt, die globale Variablen definiert, die in der Anwendung verwendet werden. Diese Variablen umfassen `DisposedValue`, `Contents`, `DataList`, `FileName` und `FileHeader`.

--- CsvFileManager/CsvFile.vb
+++ CsvFileManager/CsvFile.vb
@@ -1,6 +1,22 @@
 ﻿'****************************************************************************************************************
 'CsvFile.vb
 '(c) 2022 - 2024 by Andreas Sauer
+'
+' Diese Datei enthält die Definition der Klasse CsvFile, die zum Verwalten von CSV-Dateien verwendet wird.
+' Die Klasse bietet Methoden zum Lesen, Schreiben, Suchen, Hinzufügen, Ersetzen und Löschen von Datensätzen
+' in einer CSV-Datei. Außerdem implementiert sie das IDisposable-Interface zur ordnungsgemäßen Freigabe von Ressourcen.
+'
+' Enthaltene Methoden und Eigenschaften:
+' - Data: Gibt die Datensätze als Liste zurück.
+' - New(File As String, Header As String): Konstruktor zum Erstellen einer neuen Instanz der Klasse.
+' - FindRecord(SearchData As String): Sucht einen Begriff in der Datenbank und gibt den Index zurück.
+' - GetRecord(Index As Integer): Gibt den Datensatz mit dem angegebenen Index zurück.
+' - AddRecord(DataRecord As String): Fügt einen neuen Datensatz an das Ende der Datenbank an.
+' - ReplaceValue(Index As Integer, NewValue As String): Ersetzt einen Datensatz durch einen neuen Wert.
+' - DeleteRecord(Index As Integer): Löscht einen Datensatz mit dem angegebenen Index.
+' - Dispose(): Führt anwendungsspezifische Aufgaben zur Freigabe von Ressourcen durch.
+' - Dispose(disposing As Boolean): Bereinigt die Ressourcen, die von der Klasse verwendet werden.
+'
 '****************************************************************************************************************
 '
 
@@ -11,60 +27,44 @@ Public Class CsvFile
 
 	Implements IDisposable
 
-	Private DisposedValue As Boolean
-	Private Contents As String()
-	Private DataList As New List(Of String)
-	Private ReadOnly File As String
-	Private ReadOnly Header As String
-
 	''' <summary>
 	''' Gibt die Datensätze als Liste zurück.
 	''' </summary>
-	''' <returns></returns>
-	Public ReadOnly Property Data As List(Of String)
+	''' <returns>Liste der Datensätze</returns>
+	Public Shared ReadOnly Property Data As List(Of String)
 		Get
-			Return Me.DataList
+			Return DataList
 		End Get
 	End Property
 
 	''' <summary>
 	''' Erstellt eine neue Instanz der Klasse.
 	''' </summary>
-	''' <param name="File">
-	''' Name und Pfad der Datendatei.
-	''' </param>
-	''' <param name="Header">
-	''' Headerdaten
-	''' </param>
+	''' <param name="File">Name und Pfad der Datendatei.</param>
+	''' <param name="Header">Headerdaten</param>
 	Public Sub New(File As String, Header As String)
-		Me.File = File
-		Me.Header = Header
+		FileName = File
+		FileHeader = Header
 		'Datei erstellen falls sie nicht existiert
 		If Not IO.File.Exists(File) Then
-			Me.CreateFile()
+			CreateFile()
 		End If
 		'Datei einlesen
-		Me.ReadFile()
+		ReadFile()
 	End Sub
 
 	''' <summary>
 	''' Sucht den Begriff <paramref name="SearchData"/> in der gesamten Datenbank.
 	''' </summary>
-	''' <param name="SearchData">
-	''' Der in der Datenbank zu suchende Begriff.
-	''' </param>
-	''' <returns>
-	''' Gibt den Index des gefundenen Datensatzes zurück.
-	''' </returns>
-	''' <remarks>
-	''' Falls der Begriff <paramref name="SearchData"/> nicht gefunden wird gibt die Funktion -1 zurück.
-	''' </remarks>
-	Public Function FindRecord(SearchData As String) As Integer
+	''' <param name="SearchData">Der in der Datenbank zu suchende Begriff.</param>
+	''' <returns>Gibt den Index des gefundenen Datensatzes zurück.</returns>
+	''' <remarks>Falls der Begriff <paramref name="SearchData"/> nicht gefunden wird gibt die Funktion -1 zurück.</remarks>
+	Public Shared Function FindRecord(SearchData As String) As Integer
 		Dim result As Integer = -1
 		'Alle Datensätze nach den Suchdaten durchsuchen
-		For i As Integer = 0 To Me.DataList.Count - 1
+		For i As Integer = 0 To DataList.Count - 1
 			'Wenn gefunden -> Index merken
-			If InStr(Me.DataList.ElementAt(i), SearchData) <= 0 Then
+			If InStr(DataList.ElementAt(i), SearchData) <= 0 Then
 				Continue For
 			End If
 			result = i
@@ -77,70 +77,41 @@ Public Class CsvFile
 	''' <summary>
 	''' Gibt den Datensatz mit dem Index <paramref name="Index"/> zurück.
 	''' </summary>
-	''' <param name="Index">
-	''' Index des Datensatzes
-	''' </param>
-	Public Function GetRecord(Index As Integer) As String
-		Return Me.DataList.ElementAt(Index)
+	''' <param name="Index">Index des Datensatzes</param>
+	''' <returns>Der Datensatz</returns>
+	Public Shared Function GetRecord(Index As Integer) As String
+		Return DataList.ElementAt(Index)
 	End Function
 
 	''' <summary>
 	''' Fügt einen neuen Datensatz <paramref name="DataRecord"/> an das Ende der Datenbank an.
 	''' </summary>
-	''' <param name="DataRecord">
-	''' Datensatz der angefügt werden soll.
-	''' </param>
-	Public Sub AddRecord(DataRecord As String)
-		Me.DataList.Add(DataRecord)
-		Me.WriteFile()
+	''' <param name="DataRecord">Datensatz der angefügt werden soll.</param>
+	Public Shared Sub AddRecord(DataRecord As String)
+		DataList.Add(DataRecord)
+		WriteFile()
 	End Sub
 
 	''' <summary>
 	''' Ersetzt den Datensatz mit dem Index <paramref name="Index"/> durch den neuen Datensatz <paramref name="NewValue"/>.
 	''' </summary>
-	''' <param name="Index">
-	''' Index des zu ersetzenden Datensatzes.
-	''' </param>
-	''' <param name="NewValue">
-	''' Der neue Datensatz.
-	''' </param>
-	Public Sub ReplaceValue(Index As Integer, NewValue As String)
-		Me.DataList.Insert(Index + 1, NewValue)
-		Me.DataList.RemoveAt(Index)
-		Me.WriteFile()
+	''' <param name="Index">Index des zu ersetzenden Datensatzes.</param>
+	''' <param name="NewValue">Der neue Datensatz.</param>
+	Public Shared Sub ReplaceValue(Index As Integer, NewValue As String)
+		DataList.Insert(Index + 1, NewValue)
+		DataList.RemoveAt(Index)
+		WriteFile()
 	End Sub
 
 	''' <summary>
 	''' Löscht den Datensatz mit dem Index <paramref name="Index"/>.
 	''' </summary>
-	''' <param name="Index">
-	''' Index des zu löschenden Datensatzes.
-	''' </param>
-	Public Sub DeleteRecord(Index As Integer)
+	''' <param name="Index">Index des zu löschenden Datensatzes.</param>
+	Public Shared Sub DeleteRecord(Index As Integer)
 		'Element mit Index löschen
-		Me.DataList.RemoveAt(Index)
+		DataList.RemoveAt(Index)
 		'Datei schreiben
-		Me.WriteFile()
-	End Sub
-
-	Private Sub CreateFile()
-		'Kopfzeilen erstellen und Datei speichern
-		Me.DataList.Add(Me.Header)
-		Me.WriteFile()
-	End Sub
-
-	Private Sub ReadFile()
-		'Datei lesen
-		Me.Contents = IO.File.ReadAllLines(Me.File)
-		'Datenliste erstellen
-		Me.DataList = Me.Contents.ToList
-	End Sub
-
-	Private Sub WriteFile()
-		'Dateiinhalt generieren
-		Me.Contents = Me.DataList.ToArray()
-		'Datei schreiben
-		IO.File.WriteAllLines(Me.File, Me.Contents)
+		WriteFile()
 	End Sub
 
 	' ' TODO: Finalizer nur überschreiben, wenn "Dispose(disposing As Boolean)" Code für die Freigabe nicht verwalteter Ressourcen enthält
@@ -160,16 +131,20 @@ Public Class CsvFile
 		GC.SuppressFinalize(Me)
 	End Sub
 
+	''' <summary>
+	''' Bereinigt die Ressourcen, die von der Klasse verwendet werden.
+	''' </summary>
+	''' <param name="disposing">Gibt an, ob verwaltete Ressourcen freigegeben werden sollen.</param>
 	Protected Overridable Sub Dispose(disposing As Boolean)
-		If Not Me.DisposedValue Then
+		If Not DisposedValue Then
 			If disposing Then
 				' TODO: Verwalteten Zustand (verwaltete Objekte) bereinigen
-				Me.DataList = Nothing
-				Me.Contents = Nothing
+				DataList = Nothing
+				Contents = Nothing
 			End If
 			' TODO: Nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer überschreiben
 			' TODO: Große Felder auf NULL setzen
-			Me.DisposedValue = True
+			DisposedValue = True
 		End If
 	End Sub
 
---
+++ CsvFileManager/HelperFuctions.vb
@@ -0,0 +1,49 @@
+﻿' ****************************************************************************************************************
+' HelperFuctions.vb
+' © 2025 by Andreas Sauer
+'
+' Dieses Modul stellt Hilfsfunktionen für das Verwalten von CSV-Dateien bereit.
+' Es enthält Methoden zum Erstellen, Lesen und Schreiben von CSV-Dateien.
+'
+' Methoden:
+' - CreateFile: Erstellt eine neue Datei mit Kopfzeilen und speichert sie.
+' - ReadFile: Liest den Inhalt der Datei und erstellt eine Datenliste.
+' - WriteFile: Schreibt den Inhalt der Datenliste in die Datei.
+'
+' ****************************************************************************************************************
+
+''' <summary>
+''' Modul, das Hilfsfunktionen für das Verwalten von CSV-Dateien bereitstellt.
+''' </summary>
+Module HelperFuctions
+
+  ''' <summary>
+  ''' Erstellt eine neue Datei mit Kopfzeilen und speichert sie.
+  ''' </summary>
+  Friend Sub CreateFile()
+    'Kopfzeilen erstellen und Datei speichern
+    DataList.Add(FileHeader)
+    WriteFile()
+  End Sub
+
+  ''' <summary>
+  ''' Liest den Inhalt der Datei und erstellt eine Datenliste.
+  ''' </summary>
+  Friend Sub ReadFile()
+    'Datei lesen
+    Contents = IO.File.ReadAllLines(FileName)
+    'Datenliste erstellen
+    DataList = Contents.ToList
+  End Sub
+
+  ''' <summary>
+  ''' Schreibt den Inhalt der Datenliste in die Datei.
+  ''' </summary>
+  Friend Sub WriteFile()
+    'Dateiinhalt generieren
+    Contents = DataList.ToArray()
+    'Datei schreiben
+    IO.File.WriteAllLines(FileName, Contents)
+  End Sub
+
+End Module
